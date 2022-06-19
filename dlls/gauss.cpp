eb9a3e802209b58ce5ffbc335a1c143fa0f996dc
Roman <30943474+RezWaki@users.noreply.github.com>
2022-06-19 14:27:58 +0300

    v13 addins

--- dlls/gauss.cpp
+++ dlls/gauss.cpp
@@ -25,7 +25,6 @@
 #include "shake.h"
 #include "gamerules.h"
 
-
 #define	GAUSS_PRIMARY_CHARGE_VOLUME	256// how loud gauss is while charging
 #define GAUSS_PRIMARY_FIRE_VOLUME	450// how loud gauss is when discharged
 
